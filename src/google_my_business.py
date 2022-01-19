58b86b108c0c8511138f1a19ba825f70bc52bad2
Leo Chan <cleojanten@hotmail.com>
2022-01-19 11:26:33 -0800

    code cleanup

--- src/google_my_business.py
+++ src/google_my_business.py
@@ -199,7 +199,6 @@ class Google_My_Business():
 
         # Get review for the location
         res_status, data_raw = self.get_request(generic_url, params=params)
-        logging.info(f'STATUS: {res_status}; CONTENT: {data_raw}')
         if res_status != 200:
             logging.error(
                 'Something wrong with {} request. Please investigate.'.format(endpoint))
